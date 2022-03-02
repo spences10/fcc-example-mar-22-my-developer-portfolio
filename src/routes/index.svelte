b7b81b99a14e226f25aec66239950ac8c4505f7e
spences10 <spences10apps@gmail.com>
2022-03-02 14:17:00 +0000

    feat: :sparkles: show graphcms data on index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,30 @@
+<script context="module">
+  import { client } from '$lib/graphql-client'
+
+  export const load = async () => {
+    const { projects } = await client.request(`query GetProjects {
+      projects {
+        name
+        slug
+        description
+        tags
+        demo
+        sourceCode
+        image {
+          url
+        }
+      }
+    }`)
+    return { props: { projects } }
+  }
+</script>
+
+<script>
+  export let projects
+</script>
+
+<pre>{JSON.stringify(projects, null, 2)}</pre>
+
 <h1>Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
