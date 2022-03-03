a0b343532ba91730e5d274fde4f8fab4bcec947d
spences10 <spences10apps@gmail.com>
2022-03-03 16:24:18 +0000

    style: :lipstick: Style projects component

--- src/lib/components/project-card.svelte
+++ src/lib/components/project-card.svelte
@@ -5,12 +5,14 @@
   export let description = ''
 </script>
 
-<article>
-  <img src={url} alt={name} />
+<article class="relative group shadow-2xl col-span-2">
+  <img class="object-cover h-full" src={url} alt={name} />
   <a href={`/projects/${slug}`}>
-    <div>
-      <h2>{name}</h2>
-      <p>{description.slice(0, 80)}</p>
+    <div
+      class="absolute bottom-0 left-0 right-0 lg:opacity-0 group-hover:opacity-100 bg-primary p-4 duration-300 text-primary-content"
+    >
+      <h2 class="font-bold lg:text-xl">{name}</h2>
+      <p class="text-sm lg:text-xl">{description.slice(0, 80)}</p>
     </div>
   </a>
 </article>
