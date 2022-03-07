4194b7c52baa1c3b207bde56441bb298725f6d6c
spences10 <spences10apps@gmail.com>
2022-03-07 19:09:19 +0000

    feat: :sparkles: add footer component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Footer from '$lib/components/footer.svelte'
   import Navbar from '$lib/components/navbar.svelte'
   import '../app.css'
 </script>
@@ -7,3 +8,4 @@
 <main class="container max-w-3xl mx-auto px-4 mb-20">
   <slot />
 </main>
+<Footer />
