318f35507b33aa5725fde3e8b371e7dcf282783d
spences10 <spences10apps@gmail.com>
2022-03-09 12:47:41 +0000

    feat: :sparkles: add theme switch
    add additionl styles for navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,19 +1,23 @@
 <script>
+  import ThemeSwitch from './theme-switch.svelte'
+
   let pages = [`projects`, `posts`, `about`]
 </script>
 
 <nav class="navbar bg-neutral mb-20 shadow-xl">
   <div class="navbar-start">
-    <p class="btn btn-ghost">
+    <p class="btn btn-ghost text-neutral-content capitalize">
       <a href="/">Portfolio and Blog</a>
     </p>
   </div>
   <div class="navbar-end">
-    <div class="dropdown dropdown-left lg:hidden">
+    <div
+      class="dropdown dropdown-left lg:hidden text-neutral-content"
+    >
       <div tabindex="0" class="m-1 btn btn-ghost">Links</div>
       <ul
         tabindex="0"
-        class="bg-base-100 text-neutral-content rounded-box shadow p-2 menu dropdown-content"
+        class="bg-base-100 text-base-content rounded-box shadow p-2 menu dropdown-content"
       >
         {#each pages as page}
           <li>
@@ -24,10 +28,11 @@
     </div>
     <ul class="hidden flex-none px-2 mx-2 lg:flex">
       {#each pages as page}
-        <li class="btn btn-ghost">
+        <li class="btn btn-ghost text-neutral-content capitalize">
           <a href={`/${page}`}>{page}</a>
         </li>
       {/each}
     </ul>
+    <ThemeSwitch />
   </div>
 </nav>
